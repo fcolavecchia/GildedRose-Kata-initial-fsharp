f124e8ad4eb25c2727b507967ad6d95d63125bd1
Flavio Colavecchia <fcolavecchia@gmail.com>
2023-05-23 15:13:04 -0300

    Bottom of class 11

--- GildedRose.UnitTests/Tests.fs
+++ GildedRose.UnitTests/Tests.fs
@@ -18,3 +18,15 @@ let ``My test`` () =
     
     Assert.AreEqual(expectedItem,Items[0])
     
+
+[<Test>]
+let ``Dexterity vest`` () =
+    let Items = new List<Item>()  
+    Items.Add({Name = "+5 Dexterity Vest"; SellIn = 10; Quality = 20})
+    let app = new GildedRose(Items)
+    app.UpdateQuality()
+    
+    let expectedItem = {Name = "+5 Dexterity Vest"; SellIn = 9; Quality = 19}
+    
+    Assert.AreEqual(expectedItem,Items[0])
+    
